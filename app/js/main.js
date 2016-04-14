ec88e65affcef1064e38884993b82eee595aab13
Ramatoulie Sanyang <misramz@yahoo.com>
2016-04-13 23:11:33 -0400

    initial JQ HW intro

---
+++ app/js/main.js
@@ -0,0 +1,39 @@
+
+//Register a Click in the sections
+$('section').click(function(event) {
+// how to figure out what area was clicked
+  var area = $(event.target);
+console.log(area[0].innerHTML);
+  console.log(area);
+});
+
+//How to Hide 2 out of 3 of them but must locate first
+var section = $(area).parent();
+var para = $(section).children('p')
+
+var shown = body[0].className.includes('shown')
+
+function hideEverything () {
+    $(section).siblings().children('p').addClass('hidden').removeClass('shown');}
+
+    if (shown) {
+// if it is shown, hide it
+  console.log('it was already shown, so we hid it');
+  $(body).addClass('hidden');
+  $(body).removeClass('shown');
+} else {
+// if it's hidden, show it
+  console.log('it was hidden, so we showed it');
+  console.log('call the hideEverything function');
+  hideEverything();
+  console.log('now add shown back to the one I want');
+  $(body).addClass('shown');
+  $(body).removeClass('hidden');
+}
+​
+
+
+
+
+
+//code to cause movement
