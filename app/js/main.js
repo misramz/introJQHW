aae8672ff41f3f15447ce4501d732bbe9aba39d4
Ramatoulie Sanyang <misramz@yahoo.com>
2016-04-15 16:58:21 -0400

    Life is good!!!

--- app/js/main.js
+++ app/js/main.js
@@ -1,36 +1,48 @@
 
+var paras = $('.container').children('p');
+
+
 //Register a Click in the sections
 $('section').click(function(event) {
 // how to figure out what area was clicked
-  var area = $(event.target);
-console.log(area[0].innerHTML);
-  console.log(area);
+  var section = $(this);
+
+  paras.addClass('hidden').removeClass('shown');
+
+  var myPara = section.next('p');
+
+  myPara.addClass('shown').removeClass('hidden');
+
+
+
+
+  //
+  //
+  //
+  // var container = $(section).parent();
+  // var para = $(container).children('p')
+  //
+  // var shown = para[0].className.includes('shown');
+  // //How to Hide 2 out of 3 of them but must locate first
+  //
+  // function hideEverything () {
+  //   $(para).next().siblings().children().addClass('hidden').removeClass('shown');}
+  //
+  //   if (shown) {
+  //     // if it is shown, hide it
+  //
+  //     $(para).addClass('hidden');
+  //     $(para ).removeClass('shown');
+  //   } else {
+  //     // if it's hidden, show it
+  //
+  //     hideEverything();
+  //
+  //     $(para).addClass('shown');
+  //     $(para).removeClass('hidden');
+  //   }
 });
 
-//How to Hide 2 out of 3 of them but must locate first
-var section = $(area).parent();
-var para = $(section).children('p')
-
-var shown = body[0].className.includes('shown')
-
-function hideEverything () {
-    $(section).siblings().children('p').addClass('hidden').removeClass('shown');}
-
-    if (shown) {
-// if it is shown, hide it
-  console.log('it was already shown, so we hid it');
-  $(para).addClass('hidden');
-  $(para ).removeClass('shown');
-} else {
-// if it's hidden, show it
-  console.log('it was hidden, so we showed it');
-  console.log('call the hideEverything function');
-  hideEverything();
-  console.log('now add shown back to the one I want');
-  $(body).addClass('shown');
-  $(body).removeClass('hidden');
-}
-​
 
 
 
